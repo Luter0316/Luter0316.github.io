bd2b096889f4657edbbc14a82841ccc36c79f81e
Vitaliy <81369898+Luter0316@users.noreply.github.com>
2024-04-10 12:57:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,60 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let item = "";
+let item = [];
+
+// Смена главной кнопки при выборе товара
+function toggleItem(btn, itemId, price){
+	let item = items.find(i => i.id === itemId);
+	if (item) {
+		let newItem = {id: itemId, price: price};
+		item.push(newItem);
+		btn.classList.add('added-to-cart');
+		btn.innerText = "Удалить из корзины";
+		let totalPrice = item.reduce((total, item) => total + item.price, 0);
+		if (totalPrice > 0) {
+			tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+			if (!tg.MainButton.isVisible) {
+				tg.MainButton.show();
+			}
+		} else {
+			tg.MainButton.hide();
+		}
+	} else {
+		let index = item.indexOf(item);
+		item.splice(index, 1);
+		btn.classList.remove('added-to-cart');
+		btn.innerText = "Добавить в корзину";
+		let totalPrice = items.reduce((total, item) => total + item.price, 0);
+		if (totalPrice > 0) {
+			tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+			if (!tg.MainButton.isVisible){
+				tg.MainButton.show();
+			}
+		} else {
+			tg.MainButton.hide();
+		}
+	}
+}
+
+// Подсчет общей цены
+function calculateTotalPrice() {
+	return items.reduce((total, item) => total + item.price, 0);
+}
+
+// Отправка данных в Telegram
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	let data = {
+		items: item,
+		totalPrice: calculateTotalPrice()
+	};
+	tg.sendData(JSON.stringify(data));
+});
+
+// Товары
+document.getElementById("btn1").addEventListener("click", function(){
+	toggleItem(this, "BananaStrips", 200)
+})
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -81,9 +134,7 @@ btn6.addEventListener("click", function(){
 });
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+
 
 
 let usercard = document.getElementById("usercard");
