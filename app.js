9f8906142d021dfa4acb6cfea513816c4fd1fe9e
Vitaliy <81369898+Luter0316@users.noreply.github.com>
2024-04-15 12:03:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = '#2cab37';
 let items = [];
 
 // Смена главной кнопки при выборе товара
-function toggleItem(btn, itemId, price){
+function toggleItem(btn, itemId, price, oldtext){
 	let item = items.find(i => i.id === itemId);
 	if (!item) {
 		let newItem = {id: itemId, price: price};
@@ -28,7 +28,8 @@ function toggleItem(btn, itemId, price){
 		let index = items.indexOf(item);
 		items.splice(index, 1);
 		btn.classList.remove('added-to-cart');
-		btn.innerText = "Добавить в корзину";
+		btn.innerText = oldtext
+		// btn.innerText = "Добавить в корзину";
 		let totalPrice = items.reduce((total, item) => total + item.price, 0);
 		if (totalPrice > 0) {
 			tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
@@ -56,18 +57,30 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 });
 
 // Товары
-document.getElementById("btn1").addEventListener("click", function(){
-	toggleItem(this, "BananaStrips", 200)
+document.getElementById("btn1_50").addEventListener("click", function(){
+	toggleItem(this, "BananaStrips", 200, '200 руб. (50гр.)')
+})
+document.getElementById("btn1_100").addEventListener("click", function(){
+	toggleItem(this, "BananaStrips", 390, '390 руб. (100гр.)')
 })
 
-document.getElementById("btn2").addEventListener("click", function(){
-	toggleItem(this, "PearStrips", 210)
+document.getElementById("btn2_50").addEventListener("click", function(){
+	toggleItem(this, "PearStrips", 210, '210 руб. (50гр.)')
+})
+document.getElementById("btn2_100").addEventListener("click", function(){
+	toggleItem(this, "PearStrips", 410, '410 руб. (100гр.)')
 })
 
-document.getElementById("btn3").addEventListener("click", function(){
-	toggleItem(this, "KiwiStrips", 250)
+document.getElementById("btn3_50").addEventListener("click", function(){
+	toggleItem(this, "KiwiStrips", 250, '250 руб. (50гр.)')
+})
+document.getElementById("btn3_100").addEventListener("click", function(){
+	toggleItem(this, "KiwiStrips", 480, '480 руб. (100гр.)')
 })
 
-document.getElementById("btn4").addEventListener("click", function(){
-	toggleItem(this, "MangoStrips", 280)
+document.getElementById("btn4_50").addEventListener("click", function(){
+	toggleItem(this, "MangoStrips", 280, '280 руб. (50гр.)')
+})
+document.getElementById("btn4_100").addEventListener("click", function(){
+	toggleItem(this, "MangoStrips", 550, '550 руб. (100гр.)')
 })
